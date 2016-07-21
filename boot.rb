6b5850091bfb1e69942cdd5eced11c98577eab04
Sergey Mostovoy <svmostovoy@gmail.com>
2016-07-21 07:17:48 +0300

    setup LE, format nil and array

--- boot.rb
+++ boot.rb
@@ -7,15 +7,22 @@ require 'sidekiq'
 require 'faraday'
 require 'pusher'
 require 'dotenv'
+require 'sinatra-logentries'
 require 'require_all'
 require_all 'lib'
 require_all 'jobs'
+
+set :environment, :production
 
 Dotenv.load
 
 Pusher.app_id = ENV['PUSHER_APP_ID'] if ENV['PUSHER_APP_ID']
 Pusher.key = ENV['PUSHER_KEY'] if ENV['PUSHER_KEY']
 Pusher.secret = ENV['PUSHER_SECRET'] if ENV['PUSHER_SECRET']
+
+configure do
+  Sinatra::Logentries.token = ENV['LOGENTRIES_TOKEN']
+end
 
 Reality.configure(:demo)
 
@@ -27,5 +34,11 @@ module Reality
         'border: 1px solid #B0C6D0; padding: 1px; cursor: pointer;'
       end
     end
+  end
+end
+
+class NilClass
+  def to_s
+    'blank'
   end
 end
--- lib/searcher.rb
+++ lib/searcher.rb
@@ -12,10 +12,22 @@ class Searcher
     @result = begin
       p query
       result = eval(query) || 'blank'
-      { result: result.respond_to?(:to_html) ? result.to_html : result.to_s }
+      { result: format_response(result) }
     rescue SyntaxError, StandardError => e
       { result: e.message, error: e.message }
     end
     @result
+  end
+
+  def format_response(result)
+    if result.respond_to?(:to_html)
+      result.to_html
+    else
+      if result.is_a?(Array)
+        result.map {|e| e.respond_to?(:to_html) ? e.to_html : e.to_s }
+      else
+        result.to_s
+      end
+    end
   end
 end
